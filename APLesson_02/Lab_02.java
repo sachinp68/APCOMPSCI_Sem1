0f5dbc0bc262191d59240a50bfeee50cfb4e4b04
sachinp68 <sachinperimbeti123@gmail.com>
2016-09-09 11:30:08 -0700

    Lab_02

--- APLesson_02/Lab_02.java
+++ APLesson_02/Lab_02.java
@@ -3,8 +3,19 @@ public class Lab_02
 	public static void main(String[]args)
 	{
 
-		int x =6;
-		int y=8;
-		System.out.println(x*y);
+		int x =2;
+		int y=4;
+		int product = x * y;
+		System.out.println("2 multiplied by 4 is " +" "+ product);
+		
+		System.out.println("Sachin Perimbeti");
+		System.out.println("13781 Nob Avenue");
+		System.out.println("Del Mar, 92014");
+		
+		int w = 2;
+		int l = 3;
+		int h = 4;
+		int RectangularArea = w*l+h*l+h*w+w*l+h*l+h*w;
+		System.out.println("The surface area of your cube is " +" "+ RectangularArea);
 	}
 }
